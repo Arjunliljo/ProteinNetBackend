c32b2733246ab7806fb0c50a484aecaa41d83297
Arjunliljo <arjun7180@gmail.com>
2024-08-10 12:55:07 +0530

    created handler factory

---
+++ api/Controllers/handlerFactory.js
@@ -0,0 +1,62 @@
+import catchAsync from "../Utilities/catchAsync.js";
+
+const getAll = (Model) => {
+  return catchAsync(async (req, res, next) => {
+    const datas = await Model.find();
+    res.status(200).json({
+      status: "Success",
+      envelop: {
+        datas,
+      },
+    });
+  });
+};
+
+const getOne = (Model) => {
+  return catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const data = await Model.findById(id);
+    res.status(200).json({
+      status: "Success",
+      message: "fetched successfully",
+      envelop: {
+        data,
+      },
+    });
+  });
+};
+
+const createOne = (Model) => {
+  return catchAsync(async (req, res, next) => {
+    const createdData = await Model.create(req.body);
+    res.status(200).json({
+      status: "Success",
+      message: "created successfully",
+      envelop: {
+        data: createdData,
+      },
+    });
+  });
+};
+
+const updateOne = (Model) => {
+  return catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+
+    const updation = req.body;
+    const updatedData = await Model.findByIdAndUpdate(id, updation, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: "Success",
+      message: "Successfully Updated",
+      envelop: {
+        data: updatedData,
+      },
+    });
+  });
+};
+
+export { getAll, getOne, createOne, updateOne };
--- api/Controllers/productController.js
+++ api/Controllers/productController.js
@@ -1,56 +1,12 @@
 import Product from "../Models/productSchema.js";
-import catchAsync from "../Utilities/catchAsync.js";
+import { createOne, getAll, getOne, updateOne } from "./handlerFactory.js";
 
-const getAllProducts = catchAsync(async (req, res, next) => {
-  const products = await Product.find();
-  res.status(200).json({
-    status: "Success",
-    data: {
-      products,
-    },
-  });
-});
+const getAllProducts = getAll(Product);
 
-const getProduct = catchAsync(async (req, res, next) => {
-  const { id: productId } = req.params;
-  const product = await Product.findById(productId);
-  res.status(200).json({
-    status: "Success",
-    message: "Product fetched successfully",
-    data: {
-      product,
-    },
-  });
-});
+const getProduct = getOne(Product);
 
-const createProduct = catchAsync(async (req, res, next) => {
-  const newProduct = await Product.create(req.body);
-  res.status(200).json({
-    status: "Success",
-    message: "Product created successfully",
-    data: {
-      newProduct,
-    },
-  });
-});
+const createProduct = createOne(Product);
 
-const updateProduct = catchAsync(async (req, res, next) => {
-  const productId = req.params.id;
-
-  const updation = req.body;
-
-  const product = await Product.findByIdAndUpdate(productId, updation, {
-    new: true,
-    runValidators: true,
-  });
-
-  res.status(200).json({
-    status: "Success",
-    message: "Successfully Updated",
-    data: {
-      product,
-    },
-  });
-});
+const updateProduct = updateOne(Product);
 
 export default { getAllProducts, createProduct, updateProduct, getProduct };
